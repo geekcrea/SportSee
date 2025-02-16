795e592a1f60dac052b08da599b98022857c05a0
geekcrea <webcrea.a@gmail.com>
2025-02-16 02:14:22 +0100

    📊 Installed Recharts and fixed main.jsx setup

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppRouter from './components/AppRouter';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Router>
+    <AppRouter />
+  </Router>,
+);
